5606927906de19ba4d0503f1e31190ea9d5f9c31
s-a <stephan.ahlf@googlemail.com>
2015-08-05 06:22:53 +0200

    https://github.com/atom/electron/issues/2404#issuecomment-127858538 :+1:

--- app/app.js
+++ app/app.js
@@ -1,6 +1,9 @@
 var path= require("path");
-
 var dir = path.join(__dirname, "..", "chrome-devtools-zerodarkmatrix-theme","theme-extension" );
-console.info(process.versions.electron);
-console.info(dir);
-var ext = require('remote').require('browser-window').addDevToolsExtension(dir);
+var ext = require('remote').require('browser-window').removeDevToolsExtension("DevTools Theme: Zero Dark Matrix");
+console.log(ext);
+
+window.setTimeout(function() {
+	ext = require('remote').require('browser-window').addDevToolsExtension(dir);
+	console.log(ext);
+}, 3000);
